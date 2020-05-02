fa0d870335bb80e0e382d8fd219cc413816d3179
nurhayati96 <ayyaweela24@gmail.com>
2020-05-02 21:09:22 +0700

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -1,14 +1,22 @@
 package operator;
 
 public class OperatorBinary {
+
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variabel
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Nurhayati";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
     public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
 
-        //operator (=) sebagai assignment/penugasan
-        //menempatkan nilai kedalam variabel
-        //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
-
-        String nama = "Nurhayati";
-        int jumlahSks = 24;
-        double indeksPrestasiKumulatif = 3.89;
     }
 }
