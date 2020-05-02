f720e931b91b8999b610e47574223ba9e08f9c11
nurhayati96 <ayyaweela24@gmail.com>
2020-05-02 21:06:29 +0700

    Operator Binary Assigment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class OperatorBinary {
+    public static void main(String[] args) {
+
+        //operator (=) sebagai assignment/penugasan
+        //menempatkan nilai kedalam variabel
+        //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+        String nama = "Nurhayati";
+        int jumlahSks = 24;
+        double indeksPrestasiKumulatif = 3.89;
+    }
+}
